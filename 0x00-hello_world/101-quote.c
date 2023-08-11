c66d33756549af5e51e968406c036ca980c00d65
bodefavour <bodefavour@gmail.com>
2023-08-10 19:48:31 -0700

    fix error 4 and 5 of task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific message to the standard error.
+ * Return: Always 1
+ */
 int main(void)
 {
     char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
