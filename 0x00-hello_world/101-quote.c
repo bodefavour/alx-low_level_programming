6bb2d2fdc0d4e189b1553bf1afb03d286fbc39c2
bodefavour <bodefavour@gmail.com>
2023-08-10 19:53:20 -0700

    fix error 4 and 5 of task 8 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,18 +5,20 @@
  * main - Entry point
  *
  * Description: Prints a specific message to the standard error.
- * Return: Always 1
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(message) - 1;
-    ssize_t written = write(2, message, len);
+	char *message = "and that piece of art is useful\""
+	                " - Dora Korpar, 2015-10-19\n";
+	ssize_t len = sizeof(
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
+	ssize_t written = write(2, message, len);
 
-    if (written != len)
-    {
-        return 1;
-    }
+	if (written != len)
+	{
+		return (1);
+	}
 
-    return 1;
+	return (1);
 }
