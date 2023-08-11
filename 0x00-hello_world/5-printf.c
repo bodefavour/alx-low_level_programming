74dfd55ce5b5b36534ae7e967c70bb3eef05eac1
bodefavour <bodefavour@gmail.com>
2023-08-10 18:46:40 -0700

    fix task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the result is a piece of art,\n");
+	Return(0);
+}
