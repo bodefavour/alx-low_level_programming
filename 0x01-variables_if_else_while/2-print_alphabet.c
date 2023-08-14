4b6684d4eea9bf26666bed2169fc96455cf6ac29
bodefavour <bodefavour@gmail.com>
2023-08-13 21:33:55 -0700

    og

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,17 +7,15 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    char letter = 'a';
-
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
-
-    putchar('\n');
-
-    return (0);
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
 }
