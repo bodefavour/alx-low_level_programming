c0fee27ddd253db6b286285c3616a6751dc935a8
bodefavour <bodefavour@gmail.com>
2023-08-13 18:30:25 -0700

    fix task 2 properly

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,18 @@
 
 int main(void)
 {
-	char low;
-
-	for (low == "a"; low <== "z"; low++)
-		putchar(low);
+	char lowercase = 'a';
+	char uppercase = 'A';
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
 	putchar('\n');
-
 	return (0);
 }
