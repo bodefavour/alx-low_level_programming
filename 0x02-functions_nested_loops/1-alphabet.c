7ec652dcbff164eb6e0bcd893914a0a5e64d338d
bodefavour <bodefavour@gmail.com>
2023-08-16 13:56:50 -0700

    fixtask 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,18 @@
-#include <stdio.h>
-#
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char c
+		for (c = 'a'; c = 'z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+	
+	return(0);
