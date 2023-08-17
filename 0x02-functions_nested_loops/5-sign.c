e5bb65a4d94810453a6df7f9faa92ca56ca78bd0
bodefavour <bodefavour@gmail.com>
2023-08-16 17:10:54 -0700

    fix task 5 7

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > '1')
 	{
-		printf("+");
 		return (1);
+		printf("+");
 	}
 	else if (n == '0')
 	{
-		printf("0");
 		return (0);
+		printf("0");
 	}
 	else
 	{
-		printf("-");
 		return (-1);
+		printf("-");
 	}
 }
