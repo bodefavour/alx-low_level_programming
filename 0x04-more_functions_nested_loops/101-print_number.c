6b88d286040434b49741823a598639b4f9f49cb3
bodefavour <bodefavour@gmail.com>
2023-08-17 20:06:54 -0700

    fix final

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,31 +1,35 @@
-#include <stdio.h>
+#include "main.h"
 
-int _putchar(char c) {
-    return write(1, &c, 1);
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+void print_number(int n)
+{
+	unsigned int m, d, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
-
-void print_number(int n) {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-
-    int divisor = 1;
-    while (n / divisor > 9) {
-        divisor *= 10;
-    }
-
-    while (divisor > 0) {
-        _putchar(n / divisor + '0');
-        n %= divisor;
-        divisor /= 10;
-    }
-}
-
-int main(void) {
-    int num = 12345;
-    print_number(num);
-
-    return 0;
-}
-
