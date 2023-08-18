08c8ac19effd9d261d14b63278af1ddf82459685
bodefavour <bodefavour@gmail.com>
2023-08-17 19:29:38 -0700

    complete all tasks

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a function that draws a diagonal line
+ * @n: integer to be inserted
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+
+	_putchar('\n')
+}
