52fc713a04678405dc47b78f12e987ab571ca91e
bodefavour <bodefavour@gmail.com>
2023-08-17 18:57:55 -0700

    fixed all betty errors

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,16 @@
 
 /**
  * print_numbers - a function that prints the numbers,
- * @g:  represented integer used 
  * Return: no return
  */
 
 void print_numbers(void)
-
 {
-	int g;
+	int a;
 
-	for (g = 48; g <= 57; g++)
+	for (a = 48; a <= 57; a++)
 	{
-		_putchar(g);
+		_putchar(a);
 	}
 
 	_putchar('\n');
