6a8f7293157bb9cad3a0d45c0e9c21daa9f76849
bodefavour <bodefavour@gmail.com>
2023-08-17 18:07:57 -0700

    fix task 1 correctly

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,7 +5,16 @@
  * @c: int to be checked
  * Return: 1 if c is a digit, 0 otherwise
  */
+
 int _isdigit(int c)
+
 {
-	return (c >= '0' && c <= '9');
+	if (c >= '48' && c <= '57')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
 }
