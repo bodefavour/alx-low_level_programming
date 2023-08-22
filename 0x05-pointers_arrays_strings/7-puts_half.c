9305abaaa20cbffd4d6f4debcd75c18357eda79a
bodefavour <bodefavour@gmail.com>
2023-08-21 18:40:00 -0700

    fix task 7 1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,12 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
 
 /**
  * puts_half - a function that prints half of a string
