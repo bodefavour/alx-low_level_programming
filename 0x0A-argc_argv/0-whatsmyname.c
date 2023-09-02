0b5ca0bff54cb4abb62eb906690bb37a351ca8f0
bodefavour <bodefavour@gmail.com>
2023-09-01 20:06:22 -0700

    fix ttask 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
 /**
  * main - prints the name of the program
  * @argc: number of array
- * @argc: array of arguments
+ * @argv: array of arguments
  *
  * Return: 0 (success)
  */
@@ -13,5 +13,5 @@ int main(int argc__attribute__((unused)), char *argv[])
 {
 	printf("%s", *argv);
 
-	return(0);
+	return (0);
 }
