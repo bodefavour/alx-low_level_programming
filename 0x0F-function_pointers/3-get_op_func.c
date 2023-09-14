37b467eb5200d0e08a99a2b22cc1e8a0d2511030
bodefavour <bodefavour@gmail.com>
2023-09-14 02:39:48 -0700

    fix current task

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,43 +1,30 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * main - Prints the result of simple operations.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * get_op_func - selects the correct function to perform
+ * the operation asked by the user.
+ * @s: char operator.
  *
- * Return: Always 0.
+ * Return: pointer to the function that corresponds to the operator.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int (*get_op_func(char *s))(int, int)
 {
-	int num1, num2;
-	char *op;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-	if (argc != 4)
+	while (i < 10)
 	{
-		printf("Error\n");
-		exit(98);
+		if (s[0] == ops->op[i])
+			break;
+		i++;
 	}
 
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
-
-	if (get_op_func(op) == NULL || op[1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	printf("%d\n", get_op_func(op)(num1, num2));
-
-	return (0);
+	return (ops[i / 2].f);
 }
