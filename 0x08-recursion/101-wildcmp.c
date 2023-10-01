22148fa1e7bd00ed4be91aed853d74cfff238bef
Bode Favour <bodefavour@gmail.com>
2023-10-01 16:06:55 -0700

    fix task 4

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,8 +4,7 @@
  * wildcmp - compares two strings.
  * @s1: string 1.
  * @s2: string 2. It can contains a * as a special character.
- * 
- * Return: 1 if are identical, 0 if not.
+ * Return: 1 if are identical, 0 if not
  *
  */
 
