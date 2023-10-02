11525f465aad3fadb2c0c0a99537d24aef171b55
Bode Favour <bodefavour@gmail.com>
2023-10-02 15:51:21 -0700

    fix task 1 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - function that prints its name, followed by a new line.
- * @argc: the number of command line arguments.
- * @argv: the array that contains the program command line arguments
+ * main - funtion that prints the number of arguments passed into it
  *
- * Return: 0 (Always) success
+ * @argc: the number of command line arguments.
+ * @argv: an array that contains the program command line arguments.
+ *
+ * Return: 0 - success.
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%s\n", argv[0]);
+	printf("%d\n", argc - 1);
 	return (0);
 }
