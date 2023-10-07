4dc9555e0907dc9e0b1d7d53895e31e863d4300c
Bode Favour <bodefavour@gmail.com>
2023-10-07 16:34:14 -0700

    fix current task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ * @b: amount of bytes to be allocated
+ *
+ * Return:  a pointer to the allocated memory
+ * if malloc fails, the malloc_checked function exits and returns 98
+ */
+
+void *malloc_checked(unsigned int b);
+{
+	char *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This is the readme file for the project: 0x0C. C - More malloc, free
